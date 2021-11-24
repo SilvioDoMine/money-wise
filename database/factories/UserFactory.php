093e9aa4fa06afd42edcb3d99256482fa28e61a2
SilvioDoMine <arthurhenrique@live.com>
2021-11-24 14:14:59 -0300

    🔨 Factory só gera usuários com documento válido.
    * Apesar de que iremos substituir esses valores nos testes unitários.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,13 +15,18 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $documentList = [
+            DocumentType::CPF_ID,
+            DocumentType::CNPJ_ID,
+        ];
+
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'document_type_id' => fn () => DocumentType::factory()->create(),
-            'document_number' => $this->faker->unique()->numerify('##############'),
+            'document_type_id' => $this->faker->randomElement($documentList),
+            'document_number' => $this->faker->unique()->numerify('###########'),
             'remember_token' => Str::random(10),
         ];
     }
